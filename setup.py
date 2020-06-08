e9d77919d1b4e64ba50ba60f87f9d33f84ef0db6
rolstenhouse <rolsthoorn12@gmail.com>
2020-06-07 21:21:33 -0700

    increment version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open (join(dirname(__file__), 'requirements.txt'), 'r') as f:
 
 setup(
     name='py-iMessage',
-    version=1.4,
+    version=1.6,
     url='https://github.com/rolstenhouse/py-imessage',
     license='MIT',
     author='Rob Olsthoorn',
